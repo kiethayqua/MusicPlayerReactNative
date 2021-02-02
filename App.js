acdd562bf498403c3ed095bb7d848f5acc0cb1bb
KietNguyen <kiethayqua@gmail.com>
2021-02-02 21:56:05 +0700

    add permission read_extenal_storage android

--- App.js
+++ App.js
@@ -2,6 +2,14 @@ import React from 'react';
 import {Text, View} from 'react-native';
 import PlayerScreen from './src/screens/Player';
 
+// import {requestMultiple, PERMISSIONS} from 'react-native-permissions';
+
+// requestMultiple([PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE]).then(
+//   (statuses) => {
+//     console.log('Storage', statuses[PERMISSIONS.ANDROID.READ_EXTERNAL_STORAGE]);
+//   },
+// );
+
 const App = () => {
   return (
     <View style={{flex: 1, alignItems: 'center'}}>
